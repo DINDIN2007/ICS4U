20d838d097b3cbf11fb7a857eea7f9e0025b17ac
Dinh Viet Luong <dinhvietluong99@gmail.com>
2024-09-11 10:20:30 -0400

    Add project files.

---
+++ ICS4U/100-Hello ICS4U.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// Dinh Viet Luong
+
+int main_100() {
+	std::cout << "Hello, ICS4U";
+	return 0;
+}
---
+++ ICS4U/ICS4U.cpp
@@ -0,0 +1,7 @@
+
+#include <iostream> // for std::cout
+
+int main1() {
+
+	return 0;
+}
